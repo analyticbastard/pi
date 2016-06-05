4eb490b4dd0142954220dabed8bdc7c0108534c6
Analytic <albrecht.allenstein@gmail.com>
2016-06-05 16:50:01 +0200

    Refactored project directories

--- project.clj
+++ project.clj
@@ -1,9 +1,22 @@
 (defproject pi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solutions to Pi tests"
+  :url "https://github.com/analyticbastard/pi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.namespace "0.2.11"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  ]
+
+  :plugins [[lein-ring "0.9.7"]
+            ]
+
+  :ring {:handler pi.server/app}
+
+  :source-paths ["src/main"]
+
+  :test-paths ["src/test"]
+
   )
