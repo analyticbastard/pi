898483a193e1d170bc04e985821c531138445346
Analytic <albrecht.allenstein@gmail.com>
2016-06-06 00:47:54 +0200

    Updated user.clj

--- src/dev/clj/user.clj
+++ src/dev/clj/user.clj
@@ -1,9 +1,15 @@
 (ns user
   (require [ring.adapter.jetty :as jetty]
-           [pi.server :as server]))
+           [pi.server :as server]
+           [clojure.tools.namespace.repl :refer [refresh]]))
 
 (comment
   (def s (jetty/run-jetty server/app {:port 3000 :join? false}))
+
   (.stop s)
   (.start s)
+  )
+
+(comment
+  (refresh)
   )
